d4a6d4c0f802c2c6720b955c952e62ab6b21906f
CristopherSE13 <cmse2018@gmail.com>
2024-03-25 12:59:21 -0500

    Ejercicio Pokedex Terminado

--- app/src/main/java/com/example/apppokemon/MainActivity.kt
+++ app/src/main/java/com/example/apppokemon/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.apppokemon.databinding.ActivityMainBinding
 import com.example.apppokemon.retrofit.PokemonApiService
 import com.example.apppokemon.retrofit.PokemonResponse
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var apiRetrofit: Retrofit
     private lateinit var adapterPokemon: AdapterPokemon
+    private var offset =0
+    private var limit= 20
+    private var puedeCargar = false
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +35,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         apiRetrofit= Retrofit.Builder().baseUrl("https://pokeapi.co/api/v2/").addConverterFactory(GsonConverterFactory.create()).build()
         adapterPokemon = AdapterPokemon()
+        binding.rvitempokemon.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy>0){
+                    val itemsVisibles = binding.rvitempokemon.layoutManager!!.childCount
+                    val itemsTotales = binding.rvitempokemon.layoutManager!!.itemCount
+                    val primerItemVisible = (binding.rvitempokemon.layoutManager!! as GridLayoutManager)
+                        .findFirstVisibleItemPosition()
+                    if (puedeCargar){
+                        if (itemsVisibles + primerItemVisible >= itemsTotales){
+                            puedeCargar= false
+                            offset= 20
+                            obtenerPokemonRetrifit()
+                        }
+                    }
+                }
+            }
+        })
         binding.rvitempokemon.layoutManager= GridLayoutManager(applicationContext,3)
         binding.rvitempokemon.adapter=adapterPokemon
 
@@ -44,12 +66,13 @@ class MainActivity : AppCompatActivity() {
     }
     private fun obtenerPokemonRetrifit(){
         var ApiService= apiRetrofit.create(PokemonApiService::class.java)
-        val pokemonResponse= ApiService.obtenerPokemon()
+        val pokemonResponse= ApiService.obtenerPokemon(offset, limit)
         pokemonResponse.enqueue(object: Callback<PokemonResponse>{
             override fun onResponse(
                 call: Call<PokemonResponse>,
                 response: Response<PokemonResponse>
             ) {
+                puedeCargar=true
                 adapterPokemon.agregarPokemon(response.body()!!.results)
             }
 
